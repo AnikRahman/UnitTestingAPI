d8ec9fb75ae4715e99a87273d500fb4fec980830
hranik7 <bestsincedayone.hranik@gmail.com>
2023-08-21 12:14:14 +0600

    Domain and Controller created

---
+++ Domain/AlicoClaims.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace UnitTestingAPI.Domain
+{
+	public class AlicoClaims
+	{
+		public Guid Id { get; set; }
+		public string? Name { get; set; }
+		public string? Designation { get; set; }
+		public string? Email { get; set; }
+		public DateTime? JoiningDate { get; set; }
+		public bool IsWorking { get; set; }
+
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public ProjectType CurrentProject { get; set; }
+	}
+
+	public enum ProjectType
+	{
+		[Display(Name = "Clics")]
+		ProjectA,
+
+		[Display(Name = "Paps")]
+		ProjectB,
+
+		[Display(Name = "Both Projects")]
+		BothProjects
+	}
+}
