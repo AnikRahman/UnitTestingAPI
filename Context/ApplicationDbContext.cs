c7212836d64f65015c30b52152db846d2a57a490
hranik7 <bestsincedayone.hranik@gmail.com>
2023-08-21 14:05:27 +0600

    Db Created

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTestingAPI.Domain;
+
+namespace UnitTestingAPI.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<AlicoClaims> Claims { get; set; }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using UnitTestingAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Db Context
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+	builder.Configuration.GetConnectionString("DefaultConnection")
+
+	));
 
 var app = builder.Build();
 
